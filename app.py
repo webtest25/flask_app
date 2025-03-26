d9164eeb17e92fc6598bc382bb43b6882490497b
webtest25 <webtest25@proton.me>
2025-03-26 21:55:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-  return 'hello from flask app!'
+  return 'Hello from flask app!'
 
 if __name__ == '__main__':
-  app.run()
+  app.run(host='0.0.0.0', port=5000)
